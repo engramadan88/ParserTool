77ecac32a97a59d81a9a6fd961b713b61fc50857
Mohammad <eng.ramadan_88@yahoo.com>
2023-04-18 04:49:55 +0300

    asd

--- create-user.sql
+++ create-user.sql
@@ -1,6 +1,24 @@
 alter session set "_oracle_script"=true;
-/
 create user parser identified by parser;
-/
 grant dba to parser;
+create table parser.usage
+(
+id number not null,
+cpu_usage number not null,
+ram_usage number not null,
+timestamp date not null
+);
+alter table parser.usage add constraint usage_pk primary key (id);
+/
+create sequence parser.usage_seq;
+create trigger parser.usage_trg
+before insert on parser.usage for each row
+declare
+begin
+    if :new.id is null then
+        select parser.usage_seq.nextval
+        into :new.id
+        from dual;
+    end if;
+end;
 /
